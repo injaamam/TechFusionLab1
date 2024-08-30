a74b71b8ebb081997c24a669ed2b268806d54580
injaamam <119157613+injaamam@users.noreply.github.com>
2024-08-30 08:18:29 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//header
+
+const navLinks=document.querySelector('.nav-links');
+const hamburger=document.querySelector('.hamburger');
+
+hamburger.addEventListener('click',function(){
+    navLinks.classList.toggle('active');
+});
+
